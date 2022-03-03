c7cde7cb40a19ef4d502038c17bc678cf396d82f
YASH KOTALWAR <72193045+YashKotalwar@users.noreply.github.com>
2022-03-03 09:15:46 +0530

    Initial Commit of math.py

---
+++ math.py
@@ -0,0 +1,22 @@
+# Add
+def add(x,y):
+    return x+y
+    
+# Subtract
+def subtract(x,y):
+    return x-y      # on master branch 
+    
+# Multiply    
+def multiply(x,y):
+    return x*y      # on Bug456
+    
+# Divide    
+def divide(x,y):    #on master
+    if y==0:
+        return Divide_Error
+    else:
+        return x/y
+        
+# square implementation
+def square(x):
+    pass 
